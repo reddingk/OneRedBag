4ce32914d71a03d12f5603f575f9a3014e550189
Kris Redding <kredding@localhost.localdomain>
2015-07-29 16:24:31 -0400

    Set up Initial CMS Retrevial

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.3'
 # Twitter Bootstrap
 gem 'bootstrap-sass','3.2.0.0'
+#JSON
+gem 'json'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/c1_m2_s3_controller.rb
+++ app/controllers/c1_m2_s3_controller.rb
@@ -1,5 +1,20 @@
 class C1M2S3Controller < ApplicationController
-  def index
+  def index	
+	#require 'json'
+	@jfile = File.read(Rails.public_path+"info.json")	
+	@jdata = JSON.parse(@jfile)
+	@bagcount = @jdata["bagcount"]
+
+	@newsfeed =  []
+	@jdata["newsfeed"].each do |newsitem|
+		@newsfeed << newsitem
+	end
+	
+	@meetup = []
+	@jdata["meetup"].each do |meets|
+		@meetup << meets
+	end
+	
 	render layout: "c1m2s3"
   end
 end
